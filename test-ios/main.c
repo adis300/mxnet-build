3e08d4e35cfdf0cc2750786df115a02b010ddde2
Disi A <adis@live.cn>
2019-05-24 07:09:24 -0400

    Redo indentation.

--- test-ios/main.c
+++ test-ios/main.c
@@ -3,8 +3,8 @@
 #include "c_predict_api.h"
 
 int main() {
-	  const char* json_str = "{}";
-	  int pred = MXPredCreate(json_str, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL);
-  	printf("Hello World:%d\n", pred);
-  	return 0;
+	const char* json_str = "{}";
+	int pred = MXPredCreate(json_str, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL);
+	printf("Hello World:%d\n", pred);
+	return 0;
 }
