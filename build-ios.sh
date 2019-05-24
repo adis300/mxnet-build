aed6ff14a28981aa2b6a6523fabc0852c2a6cf2f
Disi A <adis@live.cn>
2019-05-24 06:05:58 -0400

    Update build scripts.

--- build-ios.sh
+++ build-ios.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Remove previous build
+rm -rf dist/
+
+# Build mxnet in-case anything is not ready
 ./build-mxnet.sh
 
+# Build amalgamation
 AMALGAMATION_PATH=mxnet/amalgamation
 SOURCE_FNAME=mxnet_predict-all.cc
 BKUP_FNAME=${SOURCE_FNAME}.original
@@ -9,13 +14,19 @@ cd "${AMALGAMATION_PATH}"
 echo Path changed
 make
 
-#cp ${SOURCE_FNAME} ${BKUP_FNAME}
+# Adapt source to iOS
+cp ${SOURCE_FNAME} ${BKUP_FNAME}
 
-#cd ../..
+cd ../..
 
-#python ios_adapt.py
+python ios_adapt.py
 
-#cd ${AMALGAMATION_PATH}
-#make
-#rm -f ${SOURCE_FNAME}
-#mv ${BKUP_FNAME} ${SOURCE_FNAME}
+cd ${AMALGAMATION_PATH}
+make
+rm -f ${SOURCE_FNAME}
+mv ${BKUP_FNAME} ${SOURCE_FNAME}
+cd ../..
+
+LIB_NAME=libmxnet_predict.a
+mkdir dist
+cp ${AMALGAMATION_PATH}/${LIB_NAME} dist/${LIB_NAME}
--- ios_adapt.py
+++ ios_adapt.py
@@ -6,10 +6,13 @@ fname = "mxnet/amalgamation/mxnet_predict-all.cc"
 
 cc_text = open(fname).read()
 
-cc_text = cc_text.replace("#include <cblas.h>", "#include <Accelerate/Accelerate.h>")
+cc_text = cc_text.replace("#include <cblas.h>", "#include <Accelerate/Accelerate.h>")
 cc_text = cc_text.replace("#include <emmintrin.h>", "//#include <emmintrin.h>")
 cc_text = cc_text.replace("#if defined(__ANDROID__) || defined(__MXNET_JS__)\n#define MSHADOW_USE_SSE         0\n#endif", "#define MSHADOW_USE_SSE         0", 1)
 cc_text = cc_text.replace("#ifdef __GNUC__\n  #define MX_THREAD_LOCAL __thread\n#elif __STDC_VERSION__ >= 201112L\n  #define  MX_THREAD_LOCAL _Thread_local\n#elif defined(_MSC_VER)\n  #define MX_THREAD_LOCAL __declspec(thread)\n#endif", "#define MX_TREAD_LOCAL __declspec(thread)", 1)
+cc_text = cc_text.replace("void dposv_", "int dposv_")
+cc_text = cc_text.replace("void sposv_", "int sposv_")
+cc_text = cc_text.replace("void func##_", "int func##_")
 
 modified_source = open(fname, 'w')
 modified_source.write(cc_text)
