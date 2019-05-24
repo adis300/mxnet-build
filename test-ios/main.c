28cde3dd6969d7cea09cf6d167a5d65eb20c3bf6
Disi A <adis@live.cn>
2019-05-24 06:53:21 -0400

    Bug fix.

--- test-ios/main.c
+++ test-ios/main.c
@@ -3,7 +3,7 @@
 #include "c_predict_api.h"
 
 int main() {
-	  const char* json_str = "{}\0";
+	  const char* json_str = "{}";
 	  int pred = MXPredCreate(json_str, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL);
   	printf("Hello World:%d\n", pred);
   	return 0;
